043a7734e37fce5c34bc46a49e53c043ae438213
Keryn Knight <keryn@kerynknight.com>
2011-09-28 22:49:43 +0100

    whee, copy-pasta ObjectEditAdmin templatetag stuff to History & Delete tags.

--- adminlinks/templatetags/admin_links.py
+++ adminlinks/templatetags/admin_links.py
@@ -55,23 +55,36 @@ class ObjectEditAdmin(RequiresContextAuthMixin, InclusionTag):
             new_ctx.update(link=None)
         return new_ctx
         
-class ObjectDeleteAdmin(InclusionTag):
+class ObjectDeleteAdmin(RequiresContextAuthMixin, InclusionTag):
     name = 'admin_delete'
-    template = 'adminlinks/delete.html'
+    template = 'adminlinks/delete_link.html'
     options = Options(
         Argument('object', required=True),
-        Argument('template', default='adminlinks/delete.html', required=False),
+        Argument('template', default=None, required=False),
     )
+
+    def get_template(self, context, **kwargs):
+        if not self.valid_context(context):
+            return 'adminlinks/bad_context.html'
+       
+        opts = kwargs['object']._meta
+        if not context['request'].user.has_perm(opts.app_label + '.' + opts.get_delete_permission()):
+            return 'adminlinks/bad_context.html'
+
+        if kwargs['template'] is not None:
+            return kwargs['template']
+
+        return super(ObjectDeleteAdmin, self).get_template(context, **kwargs)
     
+   
     def get_context(self, context, object, template):
         lookups = { 'app': object._meta.app_label, 'module': object._meta.module_name,}
         args = (object.pk,)
         new_ctx = {'type': object._meta.object_name}
         try:
             new_ctx.update(link=reverse(viewname='admin:%(app)s_%(module)s_delete' % lookups, args=args))
-            self.template = template
         except NoReverseMatch:
-            new_ctx.update(link=None)
+            pass
         return new_ctx
 
         
@@ -97,13 +110,12 @@ class ObjectListAdmin(InclusionTag):
         new_ctx = {'type': object._meta.object_name}
         try:
             new_ctx.update(link=reverse(viewname='admin:%(app)s_%(module)s_changelist' % lookups))
-            self.template = template
         except NoReverseMatch:
             new_ctx.update(link=None)
         return new_ctx
 
         
-class ObjectHistoryAdmin(InclusionTag):
+class ObjectHistoryAdmin(RequiresContextAuthMixin, InclusionTag):
     ''' Allow for the look-up and display of the link to an object's history
     
     Currently the implementation calls for usage as follows:
@@ -112,20 +124,29 @@ class ObjectHistoryAdmin(InclusionTag):
     {% admin_history my_object_variable %}
     '''
     name = 'admin_history'
+    template = 'adminlinks/history_link.html'
     options = Options(
         Argument('object', required=True),
-        Argument('template', default='adminlinks/history.html', required=False),
+        Argument('template', default=None, required=False),
     )
+
+    def get_template(self, context, **kwargs):
+        if not self.valid_context(context):
+            return 'adminlinks/bad_context.html'
+         
+        if kwargs['template'] is not None:
+            return kwargs['template']
+
+        return super(ObjectHistoryAdmin, self).get_template(context, **kwargs)
     
-    def get_context(self, context, object, template):
+    def get_context(self, context, object, **kwargs):
         lookups = { 'app': object._meta.app_label, 'module': object._meta.module_name,}
         args = (object.pk,)
         new_ctx = {'type': object._meta.object_name}
         try:
             new_ctx.update(link=reverse(viewname='admin:%(app)s_%(module)s_history' % lookups, args=args))
-            self.template = template
         except NoReverseMatch:
-            new_ctx.update(link=None)
+            pass
         return new_ctx
 
 class ObjectAddAdmin(InclusionTag):
