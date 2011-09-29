3516e590b6d5b4e163a8a48eae98c0d5d3647c53
Keryn Knight <keryn@kerynknight.com>
2011-09-29 21:33:14 +0100

    refactored the tests which only work on one object ...

--- adminlinks/tests.py
+++ adminlinks/tests.py
@@ -46,7 +46,7 @@ class AdminLinksTestCase(unittest.TestCase):
         # Should be blank, because we're not doing anything.
         self.assertEqual(result, u'')
 
-class AdminEditTestCase(unittest.TestCase):
+class SingleObjectTestBase(object):
 
     def setUp(self):
         self.request = HttpRequest()
@@ -56,60 +56,57 @@ class AdminEditTestCase(unittest.TestCase):
         self.superuser = User.objects.get_or_create(username='admin', is_active=True,
             is_staff=True, is_superuser=True)[0]
 
-    def default_template(self):
-        return Template('''{% load admin_links %}{% admin_edit object %}''')
+    def default_template(self, type):
+        return Template('{%% load admin_links %%}{%% admin_%s object %%}' % type)
 
-    def specified_template(self):
-        return Template('''{% load admin_links %}{% admin_edit object "adminlinks/dummy.html" %}''')
+    def specified_template(self, type):
+        return Template('{%% load admin_links %%}{%% admin_%s object "adminlinks/dummy.html" %%}' % type)
 
-    def nonexistant_template(self):
-        return Template('''{% load admin_links %}{% admin_edit object "adminlinks/this_doesnt_exist.html" %}''')
+    def nonexistant_template(self, type):
+        return Template('{%% load admin_links %%}{%% admin_%s object "adminlinks/this_doesnt_exist.html" %%}' % type)
 
     def invalid_context(self):
         ''' Utility method for comparing a templatetag's test output to what we expect '''
         return render_to_string('adminlinks/bad_context.html', {})
 
-    def valid_context(self, object):
+    def valid_context(self, object, type, urlpart):
         ''' Simulated call to the templatetag for comparison to it's actual output '''
         object_name = object._meta.object_name
         lookups = { 
             'ns': admin.site.app_name,
             'app': object._meta.app_label,
             'module': object._meta.module_name,
+            'url': urlpart,
         }
         args = (object.pk,)
-        return render_to_string('adminlinks/edit_link.html', {
+        return render_to_string('adminlinks/%s_link.html' % type, {
             'type': object_name,
-            'link': reverse(viewname='%(ns)s:%(app)s_%(module)s_change' % lookups, args=args)
+            'link': reverse(viewname='%(ns)s:%(app)s_%(module)s_%(url)s' % lookups, args=args)
         })
     
-    def test_link_without_permissions(self):
-        '''Without appropriate permissions, make sure the link isn't even displayed'''
-        template = self.default_template()
+    def without_permission(self, type):
+        template = self.default_template(type)
         self.request.user = self.user
         context = RequestContext(self.request, {'object': self.user})
         result = template.render(context)
         self.assertEqual(result, self.invalid_context())
         
-    def test_link_with_permission(self):
-        '''With correct permissions, show the edit link with the default template'''
-        template = self.default_template()
+    def with_permission(self, type, urlpart):
+        template = self.default_template(type)
         self.request.user = self.superuser
         context = RequestContext(self.request, {'object': self.user})
         result = template.render(context)
-        self.assertEqual(result, self.valid_context(self.user))
+        self.assertEqual(result, self.valid_context(self.user, type, urlpart))
 
-    def test_link_without_requestcontext(self):
-        '''Without `request` in the context, the edit link shouldn't be displayed'''
+    def without_requestcontext(self, type):
         user = self.superuser
-        template = self.default_template()
+        template = self.default_template(type)
         context = Context({'object': user})
         result = template.render(context)
         self.assertEqual(result, self.invalid_context())
 
-    def test_with_specified_good_template(self):
-        '''When given a template which exists, here's what we expect'''
-        template = self.specified_template()
+    def with_specified_good_template(self, type):
+        template = self.specified_template(type)
         self.request.user = self.superuser
         context = RequestContext(self.request, {'object': self.user})
         result = template.render(context)
@@ -117,93 +114,8 @@ class AdminEditTestCase(unittest.TestCase):
         # where everything else does not.
         self.assertEqual(result, u'')
 
-    def test_with_specified_bad_template(self):
-        '''When given a template which doesn't exist, then we'll have a template error'''
-        template = self.nonexistant_template()
-        self.request.user = self.superuser
-        context = RequestContext(self.request, {'object': self.user})
-        # The template system will provide a TemplateSyntaxError, whose underlying
-        # type was TemplateDoesNotExist. Catching both should ensure that we
-        # got an error where we expected one.
-        try:
-            result = template.render(context)
-        except (TemplateDoesNotExist, TemplateSyntaxError), e:
-            self.assertEqual(True, True)
-
-class AdminHistoryTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.request = HttpRequest()
-        # a normal, boring user
-        self.user = User.objects.get_or_create(username='standard', is_staff=False)[0]
-        # someone who /can/ access the admin, and has permissions
-        self.superuser = User.objects.get_or_create(username='admin', is_active=True,
-            is_staff=True, is_superuser=True)[0]
-
-    def default_template(self):
-        return Template('''{% load admin_links %}{% admin_history object %}''')
-
-    def specified_template(self):
-        return Template('''{% load admin_links %}{% admin_history object "adminlinks/dummy.html" %}''')
-
-    def nonexistant_template(self):
-        return Template('''{% load admin_links %}{% admin_history object "adminlinks/this_doesnt_exist.html" %}''')
-
-    def invalid_context(self):
-        ''' Utility method for comparing a templatetag's test output to what we expect '''
-        return render_to_string('adminlinks/bad_context.html', {})
-
-    def valid_context(self, object):
-        ''' Simulated call to the templatetag for comparison to it's actual output '''
-        object_name = object._meta.object_name
-        lookups = { 
-            'ns': admin.site.app_name,
-            'app': object._meta.app_label,
-            'module': object._meta.module_name,
-        }
-        args = (object.pk,)
-        return render_to_string('adminlinks/history_link.html', {
-            'type': object_name,
-            'link': reverse(viewname='%(ns)s:%(app)s_%(module)s_history' % lookups, args=args)
-        })
-    
-    def test_link_without_permissions(self):
-        '''Without appropriate permissions, make sure the link isn't even displayed'''
-        template = self.default_template()
-        self.request.user = self.user
-        context = RequestContext(self.request, {'object': self.user})
-        result = template.render(context)
-        self.assertEqual(result, self.invalid_context())
-        
-    def test_link_with_permission(self):
-        '''With correct permissions, show the edit link with the default template'''
-        template = self.default_template()
-        self.request.user = self.superuser
-        context = RequestContext(self.request, {'object': self.user})
-        result = template.render(context)
-        self.assertEqual(result, self.valid_context(self.user))
-
-    def test_link_without_requestcontext(self):
-        '''Without `request` in the context, the edit link shouldn't be displayed'''
-        user = self.superuser
-        template = self.default_template()
-        context = Context({'object': user})
-        result = template.render(context)
-        self.assertEqual(result, self.invalid_context())
-
-    def test_with_specified_good_template(self):
-        '''When given a template which exists, here's what we expect'''
-        template = self.specified_template()
-        self.request.user = self.superuser
-        context = RequestContext(self.request, {'object': self.user})
-        result = template.render(context)
-        # Note: it's supposed to be blank because it uses a dummy template,
-        # where everything else does not.
-        self.assertEqual(result, u'')
-
-    def test_with_specified_bad_template(self):
-        '''When given a template which doesn't exist, then we'll have a template error'''
-        template = self.nonexistant_template()
+    def with_specified_bad_template(self, type):
+        template = self.nonexistant_template(type)
         self.request.user = self.superuser
         context = RequestContext(self.request, {'object': self.user})
         # The template system will provide a TemplateSyntaxError, whose underlying
@@ -215,88 +127,34 @@ class AdminHistoryTestCase(unittest.TestCase):
             self.assertEqual(True, True)
 
 
-class AdminDeleteTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.request = HttpRequest()
-        # a normal, boring user
-        self.user = User.objects.get_or_create(username='standard', is_staff=False)[0]
-        # someone who /can/ access the admin, and has permissions
-        self.superuser = User.objects.get_or_create(username='admin', is_active=True,
-            is_staff=True, is_superuser=True)[0]
-
-    def default_template(self):
-        return Template('''{% load admin_links %}{% admin_delete object %}''')
-
-    def specified_template(self):
-        return Template('''{% load admin_links %}{% admin_delete object "adminlinks/dummy.html" %}''')
-
-    def nonexistant_template(self):
-        return Template('''{% load admin_links %}{% admin_delete object "adminlinks/this_doesnt_exist.html" %}''')
-
-    def invalid_context(self):
-        ''' Utility method for comparing a templatetag's test output to what we expect '''
-        return render_to_string('adminlinks/bad_context.html', {})
-
-    def valid_context(self, object):
-        ''' Simulated call to the templatetag for comparison to it's actual output '''
-        object_name = object._meta.object_name
-        lookups = { 
-            'ns': admin.site.app_name,
-            'app': object._meta.app_label,
-            'module': object._meta.module_name,
-        }
-        args = (object.pk,)
-        return render_to_string('adminlinks/delete_link.html', {
-            'type': object_name,
-            'link': reverse(viewname='%(ns)s:%(app)s_%(module)s_delete' % lookups, args=args)
-        })
-    
+class AdminSingleObjectTestCase(SingleObjectTestBase, unittest.TestCase):
     def test_link_without_permissions(self):
         '''Without appropriate permissions, make sure the link isn't even displayed'''
-        template = self.default_template()
-        self.request.user = self.user
-        context = RequestContext(self.request, {'object': self.user})
-        result = template.render(context)
-        self.assertEqual(result, self.invalid_context())
-        
+        self.without_permission('edit')
+        self.without_permission('history')
+        self.without_permission('delete')
+
     def test_link_with_permission(self):
-        '''With correct permissions, show the edit link with the default template'''
-        template = self.default_template()
-        self.request.user = self.superuser
-        context = RequestContext(self.request, {'object': self.user})
-        result = template.render(context)
-        self.assertEqual(result, self.valid_context(self.user))
+        '''With correct permissions, show the link with the default template'''
+        self.with_permission('edit', 'change')
+        self.with_permission('history', 'history')
+        self.with_permission('delete', 'delete')
 
     def test_link_without_requestcontext(self):
-        '''Without `request` in the context, the edit link shouldn't be displayed'''
-        user = self.superuser
-        template = self.default_template()
-        context = Context({'object': user})
-        result = template.render(context)
-        self.assertEqual(result, self.invalid_context())
+        '''Without `request` in the context, the link shouldn't be displayed'''
+        self.without_requestcontext('edit')
+        self.without_requestcontext('history')
+        self.without_requestcontext('delete')
 
     def test_with_specified_good_template(self):
         '''When given a template which exists, here's what we expect'''
-        template = self.specified_template()
-        self.request.user = self.superuser
-        context = RequestContext(self.request, {'object': self.user})
-        result = template.render(context)
-        # Note: it's supposed to be blank because it uses a dummy template,
-        # where everything else does not.
-        self.assertEqual(result, u'')
-
+        self.with_specified_good_template('edit') 
+        self.with_specified_good_template('history') 
+        self.with_specified_good_template('delete') 
+    
     def test_with_specified_bad_template(self):
         '''When given a template which doesn't exist, then we'll have a template error'''
-        template = self.nonexistant_template()
-        self.request.user = self.superuser
-        context = RequestContext(self.request, {'object': self.user})
-        # The template system will provide a TemplateSyntaxError, whose underlying
-        # type was TemplateDoesNotExist. Catching both should ensure that we
-        # got an error where we expected one.
-        try:
-            result = template.render(context)
-        except (TemplateDoesNotExist, TemplateSyntaxError), e:
-            self.assertEqual(True, True)
-
+        self.with_specified_bad_template('edit')
+        self.with_specified_bad_template('history')
+        self.with_specified_bad_template('delete')
 
