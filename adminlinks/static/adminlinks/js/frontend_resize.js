80f65878286231f4db81d26c03c83a9e37e20f69
Keryn Knight <keryn@kerynknight.com>
2012-07-30 21:17:24 +0100

    get the document height, rather than the body ...

--- adminlinks/static/adminlinks/js/frontend_resize.js
+++ adminlinks/static/adminlinks/js/frontend_resize.js
@@ -1,6 +1,6 @@
 (function($p, $) {
     $(document).ready(function() {
-        var newheight = $('body').height();
+        var newheight = $(document).height();
         $p.event.trigger('frontend_editor_loaded', newheight);
     });
 })(parent.django.jQuery, django.jQuery);
