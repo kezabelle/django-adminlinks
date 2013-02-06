937afe018c3bf7850ec29e3cba2ad1ddabdc5b24
Keryn Knight <keryn@kerynknight.com>
2013-02-06 22:35:51 +0000

    admin changes, most notably to the querystring arguments, and how responses are rendered.

--- adminlinks/admin.py
+++ adminlinks/admin.py
@@ -3,7 +3,6 @@ from django.contrib.admin import helpers
 from django.contrib.admin.util import unquote
 from django.core.exceptions import PermissionDenied
 from django.db import transaction
-from django.forms.formsets import all_valid
 from django.forms.models import fields_for_model
 from django.http import Http404
 from django.shortcuts import render_to_response
@@ -15,6 +14,10 @@ from django.views.decorators.csrf import csrf_protect
 from django.utils.translation import ugettext as _
 
 csrf_protect_m = method_decorator(csrf_protect)
+
+# Note: _popup is hilariously hard coded throughout the admin.
+POPUP_VAR = '_popup'
+FRONTEND_VAR = '_frontend_editing'
 
 class AdminlinksMixin(object):
     frontend_editing = True
@@ -44,7 +47,7 @@ class AdminlinksMixin(object):
     @csrf_protect_m
     @transaction.commit_on_success
     def change_field_view(self, request, object_id, fieldname, extra_context=None):
-        "The 'change' admin view for this model."
+        """The 'change' admin view for this model."""
         model = self.model
         opts = model._meta
 
@@ -99,75 +102,117 @@ class AdminlinksMixin(object):
             'object_id': object_id,
             'original': obj,
             'show_delete': False,
-            'is_popup': "_popup" in request.REQUEST,
+            'is_popup': POPUP_VAR in request.REQUEST,
             'media': mark_safe(media),
             'errors': helpers.AdminErrorList(form, inline_formsets=[]),
-            'root_path': self.admin_site.root_path,
+            'root_path': getattr(self.admin_site, 'root_path', None),
             'app_label': opts.app_label,
             }
         context.update(extra_context or {})
         return self.render_change_form(request, context, obj=obj)
 
-    def response_change(self, request, *args, **kwargs):
-        original_response = super(AdminlinksMixin, self).response_change(request, *args, **kwargs)
-        if "_popup" not in request.REQUEST or "_frontend_editing" not in request.REQUEST:
-            return original_response
-
-        opts = self.model._meta
-        app_label = opts.app_label
-        return render_to_response([
-            "adminlinks/%s/%s/success.html" % (app_label, opts.object_name.lower()),
-            "adminlinks/%s/success.html" % app_label,
-            "adminlinks/success.html"
-        ], {})
-
-    def response_add(self, request, *args, **kwargs):
-        original_response = super(AdminlinksMixin, self).response_add(request, *args, **kwargs)
-        if "_popup" not in request.REQUEST or "_frontend_editing" not in request.REQUEST:
-            return original_response
-
-        opts = self.model._meta
-        app_label = opts.app_label
-        return render_to_response([
-            "adminlinks/%s/%s/success.html" % (app_label, opts.object_name.lower()),
-            "adminlinks/%s/success.html" % app_label,
-            "adminlinks/success.html"
-        ], {})
-
     def message_user(self, request, *args, **kwargs):
-        if "_popup" not in request.REQUEST or "_frontend_editing" not in request.REQUEST:
+        if POPUP_VAR not in request.REQUEST or FRONTEND_VAR not in request.REQUEST:
             return super(AdminlinksMixin, self).message_user(request, *args, **kwargs)
         return None
 
-    def delete_view(self, request, *args, **kwargs):
+    def get_success_templates(self, request):
+        """
+        Hook method to allow subclases to add or modify the templates provided
+        in the case of a response.
+        """
+        app_label, model_name = self.model._meta.app_label, self.model._meta.object_name.lower()
+
+        return [
+            "adminlinks/%s/%s/success.html" % (app_label, model_name),
+            "adminlinks/%s/success.html" % app_label,
+            "adminlinks/success.html"
+        ]
+
+    def response_change(self, request, obj, *args, **kwargs):
+        original_response = super(AdminlinksMixin, self).response_change(request, obj, *args, **kwargs)
+        if POPUP_VAR not in request.REQUEST or FRONTEND_VAR not in request.REQUEST:
+            return original_response
+        context = {
+            'action': {
+                'add': False,
+                'change': True,
+                'delete': False,
+                'as_string': 'change',
+            },
+            'object': {
+                'pk': obj._get_pk_val(),
+                'id': obj._get_pk_val(),
+                'original': obj,
+            }
+        }
+        context.update(self.get_response_change_context(request, obj))
+        return render_to_response(self.get_success_templates(request), context)
+
+    def response_add(self, request, obj, post_url_continue='../%s/'):
+        original_response = super(AdminlinksMixin, self).response_add(request, obj, post_url_continue)
+        if POPUP_VAR not in request.REQUEST or FRONTEND_VAR not in request.REQUEST:
+            return original_response
+        context = {
+            'action': {
+                'add': True,
+                'change': False,
+                'delete': False,
+                'as_string': 'add',
+            },
+            'object': {
+                'pk': obj._get_pk_val(),
+                'id': obj._get_pk_val(),
+                'original': obj,
+            }
+        }
+        context.update(self.get_response_add_context(request, obj))
+        return render_to_response(self.get_success_templates(request), context)
+
+    def delete_view(self, request, object_id, extra_context=None):
         """Ridiculously, there's no response_delete method to patch, so instead
         we're just going to do a similar thing and hope for the best.
         """
-        #import pdb; pdb.set_trace()
         # silly Django, not providing the same variables to everything!
-        extra_context = kwargs['extra_context'] or {}
-        extra_context.update({'is_popup': "_popup" in request.REQUEST})
-        kwargs['extra_context'] = extra_context
-        resp = super(AdminlinksMixin, self).delete_view(request, *args, **kwargs)
+        our_extra_context = {'is_popup': POPUP_VAR in request.REQUEST}
+        if extra_context is None:
+            extra_context = {}
+        extra_context.update(our_extra_context)
+        resp = super(AdminlinksMixin, self).delete_view(request, object_id, extra_context)
 
         # Hijack the redirect on success to instead present our JS enabled template.
         if resp.status_code in (302,):
-            opts = self.model._meta
-            app_label = opts.app_label
-            resp = render_to_response([
-                "adminlinks/%s/%s/success.html" % (app_label, opts.object_name.lower()),
-                "adminlinks/%s/success.html" % app_label,
-                "adminlinks/success.html"
-            ], {})
+            context = {
+                'action': {
+                    'add': False,
+                    'change': False,
+                    'delete': True,
+                    'as_string': 'delete',
+                },
+                'object': {
+                    'pk': object_id,
+                    'id': object_id,
+                }
+            }
+            context.update(self.get_response_delete_context(request, object_id))
+            resp = render_to_response(self.get_success_templates(request), context)
         return resp
 
 
     def history_view(self, request, *args, **kwargs):
         #import pdb; pdb.set_trace()
         # silly Django, not providing the same variables to everything!
-        extra_context = {'is_popup': "_popup" in request.REQUEST}
+        extra_context = {'is_popup': POPUP_VAR in request.REQUEST}
         if 'extra_context' in kwargs:
             extra_context.update(kwargs['extra_context'])
         kwargs['extra_context'] = extra_context
         return super(AdminlinksMixin, self).history_view(request, *args, **kwargs)
 
+    def get_response_add_context(self, request, obj):
+        return {}
+
+    def get_response_change_context(self, request, obj):
+        return {}
+
+    def get_response_delete_context(self, request, obj_id):
+        return {}
