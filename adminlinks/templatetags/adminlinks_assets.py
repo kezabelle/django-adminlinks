98f79fe0a4760830c91f6be78493faab28f324ce
Keryn Knight <keryn@kerynknight.com>
2013-02-06 22:30:51 +0000

    naming the tags this way allows PyCharm to find them.

--- adminlinks/templatetags/adminlinks_assets.py
+++ adminlinks/templatetags/adminlinks_assets.py
@@ -7,33 +7,31 @@ register = Library()
 
 
 class AdminlinksCssShortcut(InclusionTag):
-    name = 'render_adminlinks_css'
     template = 'adminlinks/css.html'
 
     def get_context(self, context):
         """
         Updates the *existing* context, putting a boolean onto it.
-        
+
         Always returns the existing context.
         """
         result = context_passes_test(context)
         context.update({'should_load_assets': result})
         return context
-register.tag(AdminlinksCssShortcut)
+register.tag(name='render_adminlinks_css', compile_function=AdminlinksCssShortcut)
 
 
 class AdminlinksJsShortcut(InclusionTag):
-    name = 'render_adminlinks_js'
     template = 'adminlinks/js.html'
 
     def get_context(self, context):
         """
         Updates the *existing* context, putting a boolean onto it.
-        
+
         Always returns the existing context.
         """
         result = context_passes_test(context)
         context.update({'should_load_assets': result})
         return context
-register.tag(AdminlinksJsShortcut)
+register.tag(name='render_adminlinks_js', compile_function=AdminlinksJsShortcut)
 
