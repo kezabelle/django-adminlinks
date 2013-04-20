dc3bf08928c1cf706ae9758a8aeee2447d57d57b
Keryn Knight <keryn@kerynknight.com>
2013-04-20 14:35:26 +0100

    move and rename `_resort_modeladmins_for_toolbar`

--- adminlinks/templatetags/adminlinks_toolbar.py
+++ adminlinks/templatetags/adminlinks_toolbar.py
@@ -1,23 +1,13 @@
 # -*- coding: utf-8 -*-
-from collections import defaultdict
 from classytags.arguments import Flag, StringArgument
 from classytags.core import Options
 from django.template.base import Library
 from classytags.helpers import InclusionTag
 from adminlinks.templatetags.utils import (context_passes_test, get_admin_site,
-                                           get_registered_modeladmins)
+                                           get_registered_modeladmins,
+                                           _resort_modeladmins)
 
 register = Library()
-
-def _resort_modeladmins_for_toolbar(modeladmins):
-    unsorted_resultset = defaultdict(list)
-    for keypair, datavalues in modeladmins.items():
-        app, model = keypair
-        unsorted_resultset[app].append(datavalues)
-
-    resultset = list(unsorted_resultset.items())
-    resultset.sort(key=lambda x: x[0])
-    return resultset
 
 
 class AdminlinksToolbar(InclusionTag):
@@ -46,7 +36,7 @@ class AdminlinksToolbar(InclusionTag):
             context.update({
                 'should_display_toolbar': True,
                 'should_display_apps': with_labels,
-                'app_list': _resort_modeladmins_for_toolbar(modeladmins),
+                'app_list': _resort_modeladmins(modeladmins),
             })
         return context
 register.tag(name='render_adminlinks_toolbar', compile_function=AdminlinksToolbar)
