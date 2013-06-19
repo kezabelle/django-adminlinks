14b31fa4eb0ba8c530c98076bedd66a6cc751caf
Keryn Knight <keryn@kerynknight.com>
2013-06-19 20:23:01 +0100

    introduce a context processor that gets around Django #20288 and #20302 (the latter of which is BDFL'd, stubbornly and with no concrete reason)
    Enable it by adding it to TEMPLATE_CONTEXT_PROCESSORS. Attempts to contaminate as few contexts as possible by checking the view's attributes.

---
+++ adminlinks/context_processors.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import logging
+from django.core.urlresolvers import resolve
+
+logger = logging.getLogger(__name__)
+
+
+def fix_admin_popups(request):
+    valid_value = {'is_popup': True}
+    invalid_value = {}
+    if not hasattr(request, 'user'):
+        logger.debug("No user on request, probably don't need to fix popups")
+        return invalid_value
+
+    if request.user.is_anonymous():
+        logger.debug("user is anonymous; no point trying to fix popups as "
+                     "they're not signed in.")
+        return invalid_value
+
+    try:
+        resolved_func = resolve(request.path)
+        real_func = resolved_func.func.func_closure[0].cell_contents
+    except (AttributeError, IndexError) as e:
+        logger.info('Resolved function is not wrapped the way I think admin '
+                    'views should be...')
+        return invalid_value
+
+    if hasattr(real_func, 'admin_site'):
+        logger.debug("ModelAdmin instance assumed; adding `is_popup` to context")
+        return valid_value
+
+    if hasattr(real_func, 'i18n_javascript') and hasattr(real_func, '_registry'):
+        logger.debug("AdminSite instance assumed; adding `is_popup` to context")
+        return valid_value
+    #: Everything failed, so lets avoid
+    #: "other_dict must be a mapping (dictionary-like) object."
+    return invalid_value
