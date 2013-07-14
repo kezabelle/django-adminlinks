b3c7fe0d9a3afa6a0a057a892ace1f8adc5e8986
Keryn Knight <keryn@kerynknight.com>
2013-07-14 14:12:22 +0100

    change JS search namespace

--- adminlinks/static/adminlinks/js/adminlinks.js
+++ adminlinks/static/adminlinks/js/adminlinks.js
@@ -4,16 +4,14 @@
     // when we close the popup, we check it to decide whether to reload.
     window.__data_changed__ = false;
 
-    var $to_iframe = $('.admin-add, .admin-edit, .admin-delete, .admin-history, .admin-toolbar-link');
+    var $to_iframe = $('.django-adminlinks--btn');
     var $doc = $(document);
     var $body = $('body', $doc).eq(0);
     var on_popup_close = function(event, action, data) {
         if (window.__data_changed__ === true) {
             window.location.reload();
         }
-    }
-
-
+    };
     $doc.bind('fancyiframe-close', on_popup_close);
 
     var toggle_editing = function(event) {
@@ -22,7 +20,7 @@
         $me.toggleClass('icon-cog-circled')
             .toggleClass('icon-lock-circled');
         $body.toggleClass('admin-editing');
-    }
+    };
     $('.admin-toolbar-toggle .icon-cog-circled').bind('click', toggle_editing);
 
     if (window.frameElement === null) {
