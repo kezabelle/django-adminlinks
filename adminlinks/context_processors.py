9bbada81eb560f56c3f9678b4dca4c6192bb2580
Keryn Knight <keryn@kerynknight.com>
2013-07-30 17:47:19 +0100

    context processor is now more sensible, and keeps track of whether it was really a popup

--- adminlinks/context_processors.py
+++ adminlinks/context_processors.py
@@ -1,43 +1,35 @@
 # -*- coding: utf-8 -*-
 import logging
-from django.core.urlresolvers import resolve
+from django.contrib import admin
+from django.core.urlresolvers import reverse, NoReverseMatch
 
 logger = logging.getLogger(__name__)
 
 
 def fix_admin_popups(request):
-    valid_value = {'is_popup': '_popup' in request.REQUEST or
-                               'pop' in request.GET}
+    valid_value = {'is_popup': True, 'is_admin_view': True,
+                   'is_really_popup': '_popup' in request.REQUEST or
+                                      'pop' in request.GET}
     invalid_value = {}
     if not hasattr(request, 'user'):
         logger.debug("No user on request, probably don't need to fix popups")
         return invalid_value
 
-    if request.user.is_anonymous():
+    if not request.user.is_authenticated():
         logger.debug("user is anonymous; no point trying to fix popups as "
                      "they're not signed in.")
         return invalid_value
 
-    if request.path.startswith(('/admin/', '/administration/', '/backend/')):
-        logger.info("Common admin URL prefix detected, assuming it's "
-                    "an AdminSite")
-        return valid_value
-
     try:
-        resolved_func = resolve(request.path)
-        real_func = resolved_func.func.func_closure[0].cell_contents
-    except (AttributeError, IndexError) as e:
-        logger.info('Resolved function is not wrapped the way I think admin '
-                    'views should be...')
+        url_prefix = reverse('%s:index' % admin.site.name)
+    except NoReverseMatch as e:
+        logger.info('admin is not mounted')
         return invalid_value
 
-    if hasattr(real_func, 'admin_site'):
-        logger.debug("ModelAdmin instance assumed; adding `is_popup` to context")
-        return valid_value
+    if not request.path.startswith(url_prefix):
+        logger.debug("Request path {path} is not within the admin "
+                     "mounted under {admin}".format(path=request.path,
+                                                    admin=url_prefix))
+        return invalid_value
 
-    if hasattr(real_func, 'i18n_javascript') and hasattr(real_func, '_registry'):
-        logger.debug("AdminSite instance assumed; adding `is_popup` to context")
-        return valid_value
-    #: Everything failed, so lets avoid
-    #: "other_dict must be a mapping (dictionary-like) object."
-    return invalid_value
+    return valid_value
