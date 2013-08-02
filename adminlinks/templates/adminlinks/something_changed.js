c56fafad246364734052f661b64b0ef664c90972
Keryn Knight <keryn@kerynknight.com>
2013-08-02 17:58:37 +0100

    attempt to put the __data_changed__ element in, better.

---
+++ adminlinks/templates/adminlinks/something_changed.js
@@ -0,0 +1,6 @@
+;(function() {
+    window.__data_changed__ = true;
+    if (window.frameElement !== void(0) && window.frameElement !== null) {
+        parent.window.__data_changed__ = true;
+    }
+})();
