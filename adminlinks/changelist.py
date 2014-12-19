683baad8f0287ead79907bb06e99b11024214e1f
Keryn Knight <keryn@kerynknight.com>
2014-12-19 11:23:08 +0000

    `get_query_set` disappears in Django 1.8 (tested with 5b1fb0a75d6961d78ba68c72008f1cc535f9689a), so we need to smash in a duplicate method going forward.

--- adminlinks/changelist.py
+++ adminlinks/changelist.py
@@ -20,10 +20,15 @@ class AdminlinksChangeListMixin(object):
         for x in self.tracks_querystring_keys:
             if x in self.params:
                 del self.params[x]
-        return super(AdminlinksChangeListMixin, self).get_query_set(*args,
-                                                                    **kwargs)
+        return super(AdminlinksChangeListMixin, self).get_queryset(*args, **kwargs)
 
-    get_query_set = get_queryset
+    def get_query_set(self, *args, **kwargs):
+        # this should stop the default ChangeList setting e=1 when we try
+        # and set _data_changed=1 in the querystring.
+        for x in self.tracks_querystring_keys:
+            if x in self.params:
+                del self.params[x]
+        return super(AdminlinksChangeListMixin, self).get_query_set(*args, **kwargs)
 
 
 class AdminlinksChangeList(AdminlinksChangeListMixin, ChangeList):
