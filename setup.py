03525188de0326a52b1ec980090262cb59094a7c
Keryn Knight <keryn@kerynknight.com>
2015-05-17 09:37:18 +0100

    sorting some metadata

--- setup.py
+++ setup.py
@@ -5,22 +5,17 @@ from setuptools import find_packages
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 
-def tidy_requirements(requirement_file):
-    """
-    simplistic parsing of our requirements files to generate dependencies for
-    the setup file.
-    """
-    outdata = []
-    with open(requirement_file) as dependencies:
-        for line in dependencies:
-            line = line.strip()
-            # make sure any hard pinned versions are instead treated as loose
-            # minimum versions, see
-            # http://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
-            line = line.replace('==', '>=')
-            if line and not line.startswith('#') and line not in outdata:
-                outdata.append(line)
-    return outdata
+def make_readme(root_path):
+    FILES = ('README.rst', 'LICENSE', 'CHANGELOG', 'CONTRIBUTORS')
+    for filename in FILES:
+        filepath = os.path.realpath(os.path.join(root_path, filename))
+        if os.path.isfile(filepath):
+            with open(filepath, mode='r') as f:
+                yield f.read()
+
+
+LONG_DESCRIPTION = "\r\n\r\n----\r\n\r\n".join(make_readme(HERE))
+
 
 setup(
     name='django-adminlinks',
@@ -28,22 +23,25 @@ setup(
     description='Django template tags for rendering links to the '
                 'administration.',
     author='Keryn Knight',
-    author_email='github@kerynknight.com',
+    author_email='python-package@kerynknight.com',
     license="BSD License",
     keywords="django",
-    long_description=open(os.path.join(HERE, 'README.rst')).read(),
+    long_description=LONG_DESCRIPTION,
     url='https://github.com/kezabelle/django-adminlinks',
     packages=find_packages(exclude=['docs*']),
-    install_requires=tidy_requirements(os.path.join(HERE, 'adminlinks',
-                                                    'requirements.txt')),
+    install_requires=(
+        'Django>=1.4',
+        'django-classy-tags>=0.6.1',
+    ),
     include_package_data=True,
+    zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Natural Language :: English',
         'Topic :: Internet :: WWW/HTTP :: Site Management',
